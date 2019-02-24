20fa984fdbec83d02104952ce36b59a022e68dbc
rmirzojonov <ravshanjonmir@gmail.com>
2019-02-24 23:14:46 +0300

    Add project files.

---
+++ Crawler/HtmlParser.cs
@@ -0,0 +1,70 @@
+﻿using HtmlAgilityPack;
+using System.Collections.Generic;
+
+namespace Crawler
+{
+    public class HtmlParser
+    {
+        private readonly string _baseUrl;
+        private readonly HtmlWeb _web;
+        private HtmlDocument _doc;
+
+        private static readonly string[] _blackList =
+        {
+            ".jpg",
+            ".png",
+            ".gif",
+            ".mp4",
+            ".pdf"
+        };
+
+        public HtmlParser(string baseUrl)
+        {
+            _baseUrl = baseUrl;
+            _web = new HtmlWeb();
+        }
+
+        private bool IsBlacklisted(string url)
+        {
+            foreach(var black in _blackList)
+            {
+                if (url.EndsWith(black))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public void Load(string url = null)
+        {
+            _doc = _web.Load(url ?? _baseUrl);
+        }
+
+        public IEnumerable<string> GetAnchors()
+        {
+            var anchors = _doc.DocumentNode.SelectNodes("//a");
+            if(anchors is null)
+                yield break;
+
+            foreach (var anchor in anchors)
+            {
+                string href = anchor.GetAttributeValue("href", string.Empty);
+                if (!string.IsNullOrEmpty(href))
+                {
+                    if (href.StartsWith(_baseUrl) && !IsBlacklisted(href))
+                        yield return href;
+                    else
+                    {
+                        if (href.StartsWith("/"))
+                            yield return _baseUrl + href;
+                    }
+                }
+            }
+        }
+
+        public string GetContents()
+        {
+            return _doc.DocumentNode.InnerText;
+        }
+    }
+}
---
+++ Crawler/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Crawler
+{
+    class Program
+    {
+        private const string BaseUrl = "https://tutorialzine.com";
+        private static string BaseFile;
+        private static string DocumentDirectory;
+
+        private static Queue<string> _urls = new Queue<string>();
+        private static HashSet<string> _crawledUrls;
+        private static HtmlParser _parser = new HtmlParser(BaseUrl);
+
+        static string GetCurrentDirectory()
+        {
+            return Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        }
+
+        static void Initialize()
+        {
+            DocumentDirectory = GetCurrentDirectory() + "/docs";
+            Directory.CreateDirectory(DocumentDirectory);
+            BaseFile = DocumentDirectory + "/index.txt";
+
+            if (!File.Exists(BaseFile))
+                File.Create(BaseFile).Close();
+
+            _crawledUrls = new HashSet<string>(File.ReadAllLines(BaseFile));
+        }
+
+        static void Main(string[] args)
+        {
+            Initialize();
+
+            using (StreamWriter baseWriter = File.AppendText(BaseFile))
+            {
+                Crawl(BaseUrl, baseWriter);
+
+                while (_urls.Count != 0)
+                {
+                    if (_crawledUrls.Count > 100)
+                        break;
+                    string url = _urls.Dequeue();
+                    Crawl(url, baseWriter);
+                }
+            }
+
+            Console.WriteLine("\nCompleted!");
+            Console.ReadKey();
+        }
+
+        static void Crawl(string url, StreamWriter baseWriter)
+        {
+            baseWriter.WriteLine(url);
+            _crawledUrls.Add(url);
+            _parser.Load(url);
+
+            foreach (var anchor in _parser.GetAnchors())
+            {
+                if (!_crawledUrls.Contains(anchor) && !_urls.Contains(anchor))
+                {
+                    _urls.Enqueue(anchor);
+                    Console.WriteLine(anchor);
+                }
+            }
+
+            string filename = $"{DocumentDirectory}/{_crawledUrls.Count}.txt";
+            using (StreamWriter writer = File.AppendText(filename))
+            {
+                writer.Write(_parser.GetContents());
+            }
+        }
+    }
+}
